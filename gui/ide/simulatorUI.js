660b8e75ef59e53ad7c0b7eb8bb6bf416613052a
AgusLezcano <agustinmatias9@gmail.com>
2020-10-30 16:02:32 -0300

    Creo una nueva tabla de pins

--- gui/ide/simulatorUI.js
+++ gui/ide/simulatorUI.js
@@ -6,42 +6,13 @@ function noesunacopiaSimulator()
   };
   return graphics;
 }
-const test = (
-<table className="table table-sm table-dark">
-<thead>
-  <tr>
-    <th scope="col">#</th>
-    <th scope="col">Estado</th>
-    <th scope="col">Last</th>
-    <th scope="col">Handle</th>
-  </tr>
-</thead>
-<tbody>
-  <tr>
-    <th scope="row">1</th>
-    <td>Mark</td>
-    <td>Otto</td>
-    <td>@mdo</td>
-  </tr>
-  <tr>
-    <th scope="row">2</th>
-    <td>Jacob</td>
-    <td>Thornton</td>
-    <td>@fat</td>
-  </tr>
-  <tr>
-    <th scope="row">3</th>
-    <td colSpan="2">Larry the Bird</td>
-    <td>@twitter</td>
-  </tr>
-</tbody>
-</table>);
+
 function Pins(props)
 {
-  Informacion = props.pins.map((item, index) => { return (
+  var Informacion = props.pins.map((item, index) => { return (
     <tr>
       <th scope = "row">{index}</th>
-      <td>{item > 0}</td>
+      <td>{(item > 0).toString()}</td>
       <td>{item}</td>
     </tr>
   )})
@@ -60,5 +31,3 @@ function Pins(props)
     </table>
   );
 }
-const domContainer = document.querySelector("#pinsTable");
-ReactDOM.render(Pins,domContainer);
